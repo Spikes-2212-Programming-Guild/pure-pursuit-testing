016242b39eb47bdd079b1554c5100f336397d80c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-11 20:47:54 +0200

    first working version

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import com.spikes2212.control.FeedForwardController;
 import com.spikes2212.path.*;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -32,22 +33,24 @@ public class Robot extends TimedRobot {
       int RIGHT_MOTOR_TALON = 4;
       int RIGHT_MOTOR_VICTOR = 1;
 
-      int ENCODER_LEFT_POS = 0;
-      int ENCODER_LEFT_NEG = 1;
-      int ENCODER_RIGHT_POS = 3;
-      int ENCODER_RIGHT_NEG = 2;
+      int ENCODER_LEFT_POS = 1;
+      int ENCODER_LEFT_NEG = 0;
+      int ENCODER_RIGHT_POS = 2;
+      int ENCODER_RIGHT_NEG = 3;
   }
+
+  private Joystick leftJ = new Joystick(0), rightJ = new Joystick(1);
 
   public static Encoder right, left;
   public static IMotorController leftVictor, rightVictor, leftTalon, rightTalon;
   public static TankDrivetrain drivetrain;
   public static OdometryHandler handler;
   public static ADXRS450_Gyro gyro;
-  public static Waypoint start = new Waypoint(0,0), middle = new Waypoint(0,2),
-    end = new Waypoint(1.5, 2);
+  public static Waypoint start = new Waypoint(0,0), middle = new Waypoint(0,2)
+    , end = new Waypoint(1, 3);
   public static Path path;
   public static PurePursuitController controller;
-  public static final double kV = 0.2/3.05, kA = 0.002, kB = 0.04;
+  public static final double kV = 0.7/3.05, kA = 0.0, kB = 0.0;
 
   @Override
   public void robotInit() {
@@ -62,15 +65,19 @@ public class Robot extends TimedRobot {
       left.setDistancePerPulse(6*0.0254*Math.PI/360);
       gyro = new ADXRS450_Gyro();
       handler = new OdometryHandler(left::getDistance, right::getDistance,
-              gyro::getAngle, 0, 0);
-      path = new Path(0.15, 0.9,
-              0.05, 3.05, 3, 18, start,
-              middle, end);
-      controller = new PurePursuitController(handler, path, 0.4, 0.7);
+              gyro::getAngle, 0, 0) {
+          @Override
+          public Waypoint getWaypoint() {
+              return new Waypoint(getY(), getX());
+          }
+      };
+      path = new Path(0.015, 0.4,
+              0.02, 3.05, 3, 18, new Waypoint(0, 0), new Waypoint(0, 1.5),  new Waypoint(-1, 1.5), new Waypoint(0, 3));
+      controller = new PurePursuitController(handler, path, 0.2, 0.7);
   }
 
-  @Override
-  public void robotPeriodic() {
+    @Override
+    public void robotPeriodic() {
       handler.calculate();
       SmartDashboard.putNumber("odometry x position", handler.getX());
       SmartDashboard.putNumber("odometry y position", handler.getY());
@@ -84,19 +91,15 @@ public class Robot extends TimedRobot {
   public static double currentMaximum = Double.MIN_VALUE;
     @Override
     public void teleopPeriodic() {
-        drivetrain.tankDrive(1, -1);
-        if((-previousRate + left.getRate())/getPeriod() > currentMaximum)
-        SmartDashboard.putNumber("max acceleration", currentMaximum = (-previousRate + left.getRate())/getPeriod());
-        SmartDashboard.putNumber("period", getPeriod());
-        previousRate = left.getRate();
+        drivetrain.arcadeDrive(-leftJ.getY(), rightJ.getX());
     }
 
     @Override
     public void autonomousInit() {
         left.reset();
         right.reset();
-        handler.set(0,0);
         gyro.reset();
+        handler.set(0,0);
         controller.reset();
     }
 
@@ -110,7 +113,7 @@ public class Robot extends TimedRobot {
                   kB * (speeds[0] - left.getRate());
           double rightSpeed = rightController.calculate(speeds[1]) +
                           kB * (speeds[1] - right.getRate());
-         drivetrain.tankDrive(leftSpeed, rightSpeed);
+          drivetrain.tankDrive(leftSpeed, rightSpeed);
           SmartDashboard.putNumber("speed left", speeds[0]);
           SmartDashboard.putNumber("speed right", speeds[1]);
           SmartDashboard.putNumber("speed left converted", leftSpeed);
