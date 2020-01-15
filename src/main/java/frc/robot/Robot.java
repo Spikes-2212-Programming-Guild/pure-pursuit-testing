665f41c9a0e2cfed4c770a1efe547ba0c32efe00
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-15 20:11:19 +0200

    added command check

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,8 +10,11 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.spikes2212.command.drivetrains.OdometryDrivetrain;
 import com.spikes2212.command.drivetrains.TankDrivetrain;
+import com.spikes2212.command.drivetrains.commands.FollowPath;
 import com.spikes2212.control.FeedForwardController;
+import com.spikes2212.control.PIDVASettings;
 import com.spikes2212.path.*;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
@@ -43,27 +46,55 @@ public class Robot extends TimedRobot {
 
   public static Encoder right, left;
   public static IMotorController leftVictor, rightVictor, leftTalon, rightTalon;
-  public static TankDrivetrain drivetrain;
+  public static OdometryDrivetrain drivetrain;
   public static OdometryHandler handler;
   public static ADXRS450_Gyro gyro;
-  public static Waypoint start = new Waypoint(0,0), middle = new Waypoint(0,2)
-    , end = new Waypoint(1, 3);
   public static Path path;
   public static PurePursuitController controller;
   public static final double kV = 0.7/3.05, kA = 0.0, kB = 0.0;
+  public static FeedForwardController leftController, rightController;
 
   @Override
   public void robotInit() {
-      drivetrain = new TankDrivetrain((WPI_TalonSRX)
-              (leftTalon = new WPI_TalonSRX(RobotMap.LEFT_MOTOR_TALON))
-              , (WPI_TalonSRX)(rightTalon = new WPI_TalonSRX(RobotMap.RIGHT_MOTOR_TALON)));
-      (leftVictor = new WPI_VictorSPX(RobotMap.LEFT_MOTOR_VICTOR)).follow(leftTalon);
-      (rightVictor = new WPI_VictorSPX(RobotMap.RIGHT_MOTOR_VICTOR)).follow(rightTalon);
       right = new Encoder(RobotMap.ENCODER_RIGHT_POS, RobotMap.ENCODER_RIGHT_NEG);
       left = new Encoder(RobotMap.ENCODER_LEFT_POS, RobotMap.ENCODER_LEFT_NEG);
       right.setDistancePerPulse(6*0.0254*Math.PI/360);
       left.setDistancePerPulse(6*0.0254*Math.PI/360);
       gyro = new ADXRS450_Gyro();
+      drivetrain = new OdometryDrivetrain((WPI_TalonSRX)
+              (leftTalon = new WPI_TalonSRX(RobotMap.LEFT_MOTOR_TALON))
+              , (WPI_TalonSRX)(rightTalon = new WPI_TalonSRX(RobotMap.RIGHT_MOTOR_TALON))) {
+          private OdometryHandler handler = new OdometryHandler(left::getDistance, right::getDistance,
+                  gyro::getAngle, 0,0);
+          @Override
+          public OdometryHandler getHandler() {
+              return handler;
+          }
+
+          @Override
+          public double getWidth() {
+              return 0.7;
+          }
+
+          @Override
+          public void zeroSensors() {
+              left.reset();
+              right.reset();
+              gyro.reset();
+          }
+
+          @Override
+          public double getLeftRate() {
+              return left.getRate();
+          }
+
+          @Override
+          public double getRightRate() {
+              return right.getRate();
+          }
+      };
+      (leftVictor = new WPI_VictorSPX(RobotMap.LEFT_MOTOR_VICTOR)).follow(leftTalon);
+      (rightVictor = new WPI_VictorSPX(RobotMap.RIGHT_MOTOR_VICTOR)).follow(rightTalon);
       handler = new OdometryHandler(left::getDistance, right::getDistance,
               gyro::getAngle, 0, 0) {
           @Override
@@ -74,6 +105,10 @@ public class Robot extends TimedRobot {
       path = new Path(0.075, 0.4,
               0.6, 3.05, 3, 18, new Waypoint(0, 0), new Waypoint(0, 1.5),  new Waypoint(-1, 1.5), new Waypoint(0, 3));
       controller = new PurePursuitController(handler, path, 0.2, 0.7);
+      leftController = new FeedForwardController(kV, kA, getPeriod());
+      rightController = new FeedForwardController(kV, kA, getPeriod());
+      SmartDashboard.putData("autonomous", new FollowPath(drivetrain, path, 0.2,
+              new PIDVASettings(kB, 0, 0, kV, kA)));
   }
 
     @Override
@@ -106,8 +141,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
           double[] speeds = controller.getTargetSpeeds();
-          FeedForwardController leftController = new FeedForwardController(kV, kA, getPeriod());
-          FeedForwardController rightController = new FeedForwardController(kV, kA, getPeriod());
           double leftSpeed = leftController.calculate(speeds[0]) +
                   kB * (speeds[0] - left.getRate());
           double rightSpeed = rightController.calculate(speeds[1]) +
@@ -117,6 +150,5 @@ public class Robot extends TimedRobot {
           SmartDashboard.putNumber("speed right", speeds[1]);
           SmartDashboard.putNumber("speed left converted", leftSpeed);
           SmartDashboard.putNumber("speed right converted", rightSpeed);
-
   }
 }
